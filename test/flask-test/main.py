0ec23f5ab435825d192ff8757951ed3f41ec8642
hiroki <hiro_bass.0827@icloud.com>
2021-08-12 19:22:59 +0900

    add flask & tweepy test

--- test/flask-test/main.py
+++ test/flask-test/main.py
@@ -1,9 +1,10 @@
 import os
 import tweepy as tp
 from dotenv import load_dotenv
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
 load_dotenv()
-
 CONSUMER_API_KEY = os.environ.get("CONSUMER_API_KEY")
 CONSUMER_SECRET_API_KEY = os.environ.get("CONSUMER_SECRET_API_KEY")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
@@ -13,7 +14,18 @@ auth = tp.OAuthHandler(CONSUMER_API_KEY, CONSUMER_SECRET_API_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tp.API(auth)
 
-fav_tweets = api.favorites("@odmishien")
-for tweet in fav_tweets:
-    print("-------------------")
-    print(tweet.text)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/", methods=['POST'])
+def get_form():
+    twitter_id = request.form['user-id']
+    fav_tweets = api.favorites(twitter_id)
+    return render_template('result.html', twitter_id=twitter_id, fav_tweets=fav_tweets)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, threaded=True)
