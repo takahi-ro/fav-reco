5eba2a440cc599828fa7ba57880d108c1fbe0400
hiroki <hiro_bass.0827@icloud.com>
2021-09-04 18:13:56 +0900

    add new func

--- libs/rakuten_books_api.py
+++ libs/rakuten_books_api.py
@@ -1,14 +1,39 @@
 import requests
 
 
+APPLICATION_ID = "1095524729477042360"
+
+
 # ISBNを引数に渡すと，その本に関する情報を辞書で返す
 def getBookInfoFromISBN(isbn):
-    APPLICATION_ID = "1095524729477042360"
     api_url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404"
     params = {
             "format": "json",
             "applicationId": APPLICATION_ID,
             "isbn": isbn,
+            "hits": 1,
+            "sort": "sales"
+            }
+    results_json = requests.get(api_url, params).json()
+    book_info = results_json['Items'][0]['Item']
+    results = {
+            "title": book_info["title"],
+            "image": book_info["mediumImageUrl"],
+            "author": book_info["author"],
+            "caption": book_info["itemCaption"],
+            "sales_date": book_info["salesDate"],
+            "publisher": book_info["publisherName"],
+            "rakuten_url": book_info["itemUrl"]
+            }
+    return results
+
+
+def getBookInfoFromTitleAndAuthor(title, author):
+    api_url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404"
+    params = {
+            "format": "json",
+            "applicationId": APPLICATION_ID,
+            "title": title,
             "hits": 1,
             "sort": "sales"
             }
