0c4badf663bc04823fe12a42acc5b11a28781131
hiroki <hiro_bass.0827@icloud.com>
2021-09-05 13:34:47 +0900

    fix function in libs

--- libs/rakuten_books_api.py
+++ libs/rakuten_books_api.py
@@ -28,16 +28,20 @@ def getBookInfoFromISBN(isbn):
     return results
 
 
+# ISBNを引数に渡すと，その本に関する情報を辞書で返す
 def getBookInfoFromTitleAndAuthor(title, author):
     api_url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404"
     params = {
             "format": "json",
             "applicationId": APPLICATION_ID,
             "title": title,
+            "author": author,
+            "size": 1,
             "hits": 1,
             "sort": "sales"
             }
     results_json = requests.get(api_url, params).json()
+    
     book_info = results_json['Items'][0]['Item']
     results = {
             "title": book_info["title"],
